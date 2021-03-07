073ce14d45403a68dfe0b628ef729d909202bcee
Youmebangbang <34688253+youmebangbang@users.noreply.github.com>
2021-03-06 21:35:08 -0500

    GUI development

--- dataset_gui.py
+++ dataset_gui.py
@@ -1,7 +1,10 @@
+import numpy
+import simpleaudio as sa
 import io
 import math
 from pydub import AudioSegment
 from pydub.utils import mediainfo
+from pydub.playback import play
 from dearpygui.core import *
 from dearpygui.simple import *
 import os
@@ -14,7 +17,108 @@ import shutil
 from google.cloud import storage
 from google.cloud import speech as speech
 
-class Project:
+class Proofreader:
+    def __init__(self):
+        self.current = None
+        self.current_bak = None
+        self.current_point = None
+        self.current_plot_point = None
+        self.next = None
+        self.next_bak = None
+        self.next_point = None
+        self.next_plot_point = None
+
+    def set_rate(self, rate):
+        self.rate = rate
+
+    def get_rate(self):
+        return rate
+
+    def set_current_point(self, point):
+        self.current_point = point
+
+    def get_current_point(self):
+        return self.current_point
+
+    def set_next_point(self, point):
+        self.next_point = point
+
+    def get_next_point(self):
+        return self.next_point
+
+    def cut_wav(self, mode, wav_name, selection):
+        #do some wav cutting
+        print("cutting")
+
+    def set_current(self, wav):
+        self.current_bak = self.current
+        self.current = wav         
+    
+    def set_next(self, wav):
+        self.next = wav
+
+    def get_current(self):
+        return self.current
+
+    def get_current_bak(self):
+        return current_bak
+
+    def get_next(self):
+        return self.next
+
+    def set_project_path(self, path):
+        self.project_path = path
+
+    def get_project_path(self):
+        return self.project_path
+
+    def plot_wavs(self):
+        audio1 = self.current.get_array_of_samples()   
+        w1_int16 = numpy.frombuffer(audio1, dtype=numpy.int16)
+        w1_float32 = list(w1_int16.astype(numpy.float32))
+
+        audio2 = self.next.get_array_of_samples()   
+        w2_int16 = numpy.frombuffer(audio2, dtype=numpy.int16)
+        w2_float32 = list(w2_int16.astype(numpy.float32))
+
+        w1_x_axis = []
+        w2_x_axis = []
+
+        for i in range(0, len(w1_float32)):
+            w1_x_axis.append(i)
+        for i in range(0, len(w2_float32)):
+            w2_x_axis.append(i)  
+
+        print("current_plot length for sample rate is: {}".format(len(w1_x_axis) / 44100))     
+
+        add_line_series("current_plot", "", w1_x_axis, w1_float32, weight=2)
+        add_line_series("next_plot", "", w2_x_axis, w2_float32, weight=2)
+
+    def current_plot_drawing_set_point(self, point):
+        self.current_point = point
+        draw_line("current_plot_drawing", [0,5], [1200, 5], [255, 255, 0, 255], 10)
+        if point:
+            draw_line("current_plot_drawing", [point-2,5], [point+2, 5], [255, 0, 0, 255], 10)
+    
+    def next_plot_drawing_set_point(self, point): 
+        self.next_point = point
+        draw_line("next_plot_drawing", [0,5], [1200, 5], [255, 255, 0, 255], 10)
+        if point:
+            draw_line("next_plot_drawing", [point-2,5], [point+2, 5], [255, 0, 0, 255], 10)
+    
+    def set_next_plot_point(self, point):
+        self.next_plot_point = point
+
+    def get_next_plot_point(self):
+        return self.next_plot_point
+
+    def set_current_plot_point(self, point):
+        self.current_plot_point = point
+
+    def get_current_plot_point(self):
+        return self.current_plot_point
+
+class Dataset_builder:
     def __init__(self, project_name, speaker_text_path, wav_file_path, index_start, cut_length, contains_punc):    
         self.project_name = project_name
         self.speaker_text_path = speaker_text_path
@@ -163,19 +267,18 @@ class Project:
                             text_out = text_out.strip() 
 
                             wav_cut = w[(beginning_cut*1000):(end_cut*1000)]
-                            new_wav_filename = str(index_count) + ".wav"                        
+                            new_wav_filename = "wavs/"  + str(index_count) + ".wav"                        
                             new_csv_file.write("{}{}|{}".format(newline, new_wav_filename, text_out))
                             wav_cut.export(output_wav_path + new_wav_filename, format="wav")
                             index_count += 1
                             newline = '\n'
 
-                        csv_file_temp.close()
-                        
+                        csv_file_temp.close()                        
                                     
                     else:
                         w = AudioSegment.from_wav(audio_name)
                         wav_cut = w[(beginning_cut*1000):(end_cut*1000)]
-                        new_wav_filename =  str(index_count) + ".wav"
+                        new_wav_filename =  "wavs/" + str(index_count) + ".wav"
                         new_csv_file.write("{}{}|{}".format(newline, new_wav_filename, text_out))
                         wav_cut.export(output_wav_path + new_wav_filename, format="wav")
                         index_count += 1
@@ -271,32 +374,27 @@ def transcribe_file(wavfile, bucket_name, project_name):
             
     set_value("label_wav_file_transcribe", "Done!")    
 
-
-def open_speaker_txt_file_callback(sender, data):
-    open_file_dialog(add_speaker_txt_file)
-
-def open_wav_file_callback(sender, data):
-    open_file_dialog(add_speaker_wav_file)    
-   
-def open_wav_file_transcribe_callback(sender, data):
+#Functions / callbacks for Google Speech
+def open_wav_file_transcribe_call(sender, data):
     open_file_dialog(add_wav_file_transcribe) 
 
 def add_wav_file_transcribe(sender, data):
     #open working wav for transcribing
     set_value("label_wav_file_transcribe", "{}/{}".format(data[0], data[1]))    
 
-def run_google_speech_callback(sender, data):
+def run_google_speech_call(sender, data):
     #run transcription
     transcribe_file(get_value("label_wav_file_transcribe"), get_value("input_storage_bucket"), get_value("input_project_name_transcribe"))     
 
-def run_dataset_builder_callback(sender, data):
+#Functions / callbacks for Dataset Builder
+def run_dataset_builder_call(sender, data):
     #check to see if txt and wav file was selected
     set_value("label_build_status", "Running builder...")
     if get_value("input_project_name") and get_value("label_speaker_text_path") and get_value("label_wav_file_path"):
         print("running")
-        myproject = Project(get_value("input_project_name"), get_value("label_speaker_text_path")
+        builder = Dataset_builder(get_value("input_project_name"), get_value("label_speaker_text_path")
         , get_value("label_wav_file_path"), get_value("input_starting_index"), get_value("input_cut_length"), get_value("input_contains_punc"))
-        myproject.build_dataset()
+        builder.build_dataset()
     else:
         print("error: enter inputs")
 
@@ -308,15 +406,151 @@ def add_speaker_wav_file(sender, data):
     #open working speaker text
     set_value("label_wav_file_path", "{}/{}".format(data[0], data[1]))
 
-set_main_window_size(1200,800)
-set_main_window_title("DeepVoice Dataset Creator 1.0 by YouMeBangBang")
+def open_speaker_txt_file_call(sender, data):
+    open_file_dialog(add_speaker_txt_file)
+
+def open_wav_file_call(sender, data):
+    open_file_dialog(add_speaker_wav_file)  
+
+#Functions / callbacks for Proofreader
+def open_csv_proofread_call(sender, data):
+    open_file_dialog(add_csv_file_proofread_call)
+
+def add_csv_file_proofread_call(sender, data):
+    path = "{}/{}".format(data[0], data[1])    
+
+    #populate table with entries
+    with open(path, 'r') as csv_file:            
+        csv_reader = csv.reader(csv_file, delimiter='|')
+        for row in csv_reader: 
+            #wav_filename should include path to wav file
+            wav_filename_with_path = row[0] 
+            text = row[1]
+            add_row("table_proofread", [wav_filename_with_path, text])
+
+    w1_wav = AudioSegment.from_wav("{}/wavs/1.wav".format(data[0]))
+    w2_wav = AudioSegment.from_wav("{}/wavs/2.wav".format(data[0]))
+
+
+
+    #set project sample rate    
+    wav_info = mediainfo("{}/wavs/1.wav".format(data[0]))
+    sample_rate = wav_info['sample_rate']
+    proofreader.set_rate(sample_rate)
+    print("project rate is: {}".format(sample_rate))
+
+    proofreader.set_project_path(data[0])
+    proofreader.set_current(w1_wav)
+    proofreader.set_next(w2_wav)
+    proofreader.plot_wavs()
+
+    # audio1 = w1_wav.get_array_of_samples()   
+    # w1_int16 = numpy.frombuffer(audio1, dtype=numpy.int16)
+    # w1_float32 = list(w1_int16.astype(numpy.float32))
+
+    # audio2 = w2_wav.get_array_of_samples()   
+    # w2_int16 = numpy.frombuffer(audio2, dtype=numpy.int16)
+    # w2_float32 = list(w2_int16.astype(numpy.float32))
+
+    # w1_x_axis = []
+    # w2_x_axis = []
+
+    # for i in range(0, len(w1_float32)):
+    #     w1_x_axis.append(i)
+    # for i in range(0, len(w2_float32)):
+    #     w2_x_axis.append(i)        
+
+    # add_line_series("current_plot", "", w1_x_axis, w1_float32, weight=2)
+    # add_line_series("next_plot", "", w2_x_axis, w2_float32, weight=2)
+
+def current_restore_call(sender, data):
+    pass
+def current_export_call(sender, data):
+    pass
+def current_delete_call(sender, data):
+    w_current = proofreader.get_current()
+    w_next = proofreader.get_next()
+    point = proofreader.get_current_point()
+
+    #scale point
+    point = point
+    w_out = w_current[:point]
+    w_cut = w_current[point+1:]
+    w_next = w_cut + w_next
+
+    proofreader.set_current(w_out)
+    proofreader.set_next(w_next)
+
+def current_play_from_selection_call(sender, data):
+    w_current = proofreader.get_current()
+    point = proofreader.get_current_point()
+    w_cut = w_current[point+1:]
+    play(w_cut)
+
+def current_send_call(sender, data):
+    pass
+def next_restore_call(sender, data):
+    pass
+def next_export_call(sender, data):
+    pass
+def next_delete_call(sender, data):
+    pass
+def next_send_call(sender, data):
+    pass
+def next_play_to_selection_call(sender, data):
+    pass
+
+def current_play_call(sender, data):
+    play(proofreader.get_current())
+
+def next_play_call(sender, data):
+    play(proofreader.get_next())    
+
+def table_row_selected_call(sender, data):
+    index = get_table_selections("table_proofread")
+    set_table_selection("table_proofread", index[0][0], index[0][1], False)
+    
+    w1_wav = AudioSegment.from_wav("{}/wavs/{}.wav".format(proofreader.get_project_path(), index[0][0] + 1))
+    w2_wav = AudioSegment.from_wav("{}/wavs/{}.wav".format(proofreader.get_project_path(), index[0][0] + 2))
+
+    proofreader.set_current(w1_wav)
+    proofreader.set_next(w2_wav)
+
+    #set_item_style_var("current_plot", mvGuiStyleVar_Name, "")
+
+    proofreader.plot_wavs()
+
+
+def mouse_clicked_proofread_call(sender, data):
+    mouse_plot_pos = get_plot_mouse_pos()
+    mouse_pos = get_mouse_pos(local=False)
+    print(mouse_pos)
+    point = mouse_pos[0]
+    #offset
+    point += -7
+    if is_item_clicked("current_plot"):        
+        proofreader.current_plot_drawing_set_point(point)
+        proofreader.set_current_plot_point(mouse_plot_pos[0])         
+    else:
+        proofreader.next_plot_drawing_set_point(point)
+        proofreader.set_next_plot_point(mouse_plot_pos[0])         
+
+
+    
+
+set_main_window_size(1600, 1000)
+set_main_window_title("DeepVoice Dataset Creator / Editor 1.0 by YouMeBangBang")
 set_global_font_scale(1.5)
 
 
 set_theme("Dark")
 #set_theme_item(mvGuiCol_WindowBg, 0, 0, 200, 200)
 
+#initialize proofreader 
+proofreader = Proofreader()
+
 with window("mainWin"):
+    set_mouse_click_callback(mouse_clicked_proofread_call)
 
     with tab_bar("tb1"):
         with tab("tab0", label="Transcribe Audio"):
@@ -334,16 +568,14 @@ with window("mainWin"):
 
             add_spacing(count=5)
             add_text("Select the wav file to transcribe (must be mono)")
-            add_button("open_wav_file_transcribe", callback=open_wav_file_transcribe_callback, label="Open wav file") 
+            add_button("open_wav_file_transcribe", callback=open_wav_file_transcribe_call, label="Open wav file") 
             add_spacing(count=5)
-            add_button("run_google_speech", callback=run_google_speech_callback, label="Run Google Speech to Text") 
+            add_button("run_google_speech", callback=run_google_speech_call, label="Run Google Speech to Text") 
             add_spacing(count=5)
             add_label_text("label_wav_file_transcribe", label="")
 
 
-
         with tab("tab1", label="Build Dataset"):
-
             add_spacing(count=5)
             add_text("Enter name of project: ")
             add_same_line(spacing=10)
@@ -357,31 +589,62 @@ with window("mainWin"):
             add_same_line(spacing=10)
             add_input_text("input_cut_length", width=200, default_value="11.0", label="")            
             add_spacing(count=5)
-
             add_text("Does the text have proper punctuation? ")
             add_same_line(spacing=10)
             add_checkbox("input_contains_punc", default_value=1, label="")                         
             add_spacing(count=5)            
             add_text("Select speaker text file to open: ")
             add_same_line(spacing=10)
-            add_button("open_speaker_text", callback=open_speaker_txt_file_callback, label="Open txt file") 
+            add_button("open_speaker_text", callback=open_speaker_txt_file_call, label="Open txt file") 
             add_spacing(count=5)
             add_label_text("label_speaker_text_path", label="")
             add_spacing(count=5)
             add_text("Select speaker audio wav file to open: ")
             add_same_line(spacing=10)
-            add_button("open_wav_file", callback=open_wav_file_callback, label="Open wav file") 
+            add_button("open_wav_file", callback=open_wav_file_call, label="Open wav file") 
             add_label_text("label_wav_file_path", label="")
             add_spacing(count=5)
-            add_button("run_dataset_builder", callback=run_dataset_builder_callback, label="Run dataset builder") 
+            add_button("run_dataset_builder", callback=run_dataset_builder_call, label="Run dataset builder") 
             add_spacing(count=5)
             add_label_text("label_build_status", label="")
 
 
-
         with tab("tab2", label="Proofread Dataset"):
+            tabledata = []
             add_spacing(count=5)
             add_text("Choose a csv file to proofread and edit wavs")
+            add_spacing(count=5)
+            add_button("open_csv_proofread", callback=open_csv_proofread_call, label="Open csv file")             
+            add_table("table_proofread", ["Wav path", "Text"], callback=table_row_selected_call)
+            add_spacing(count=5)
+            add_plot("current_plot", label="Current Wav", width=1200, height=200, xaxis_no_tick_labels=True,  
+                yaxis_no_tick_labels=True, no_mouse_pos=True, crosshairs=True, xaxis_lock_min=True, xaxis_lock_max=True, yaxis_lock_min=True, yaxis_lock_max=True)
+            add_same_line(spacing=10)
+            with group("group1"):
+                add_button("current_play", callback=current_play_call, label="Play")  
+                add_button("current_play_from_selection", callback=current_play_from_selection_call, label="Play from selection")  
+                add_button("current_send", callback=current_send_call, label="Send end cut to Next")  
+                add_button("current_delete", callback=current_delete_call, label="Delete end cut")  
+                add_button("current_export", callback=current_export_call, label="Export wav")  
+                add_button("current_restore", callback=current_restore_call, label="Restore wav")  
+
+            add_drawing("current_plot_drawing", width=1200, height=16)
+            proofreader.current_plot_drawing_set_point(0)
+
+            add_spacing(count=5)
+            add_plot("next_plot", label="Next Wav", width=1200, height=200, xaxis_no_tick_labels=True, 
+                yaxis_no_tick_labels=True, no_mouse_pos=True, crosshairs=True, xaxis_lock_min=True, xaxis_lock_max=True, yaxis_lock_min=True, yaxis_lock_max=True)
+            add_same_line(spacing=10)
+            with group("group2"):
+                add_button("next_play", callback=next_play_call, label="Play")  
+                add_button("next_play_to_selection", callback=next_play_to_selection_call, label="Play to selection")  
+                add_button("next_send", callback=next_send_call, label="Send beginning cut to Current")  
+                add_button("next_delete", callback=next_delete_call, label="Delete end cut")  
+                add_button("next_export", callback=next_export_call, label="Export wav")  
+                add_button("next_restore", callback=next_restore_call, label="Restore wav")                 
+            add_drawing("next_plot_drawing", width=1200, height=10)  
+            proofreader.next_plot_drawing_set_point(0)
+
 
         with tab("tab3", label="Increase Dataset"):
             add_spacing(count=5)
