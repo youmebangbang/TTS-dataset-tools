4a5e2e02edfea2372a130e027d1d3c56d8dc38da
Youmebangbang <34688253+youmebangbang@users.noreply.github.com>
2021-03-26 16:15:32 -0400

    Update hparams.py

--- hparams.py
+++ hparams.py
@@ -86,8 +86,8 @@ def create_hparams(hparams_string=None, verbose=False):
         prenet_dim=256,
         max_decoder_steps=10000,
         gate_threshold=0.5,
-        p_attention_dropout=0.0,
-        p_decoder_dropout=0.0,
+        p_attention_dropout=0.1,
+        p_decoder_dropout=0.1,
 
         # Attention parameters
         attention_rnn_dim=1024,
